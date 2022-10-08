be0ec92d1718e44f6dd8c4d7802b00d0f73f8342
Yairys <yayi.arist.hz@gmail.com>
2022-10-08 20:20:28 -0300

    dia 3 y 4

---
+++ OneDrive/Escritorio/Python/myPython/00_hello.py
@@ -0,0 +1,17 @@
+# esto es un comentario
+#mostrar en consola con print()
+print("hello python")
+
+""" 
+Asi hago un comentario
+en varias 
+lineas
+"""
+
+#tipos de datos
+  #"string", 'string'
+#---CONSULTAR TIPO DE DATO---
+print(type("type es como el typeof, esto es clase string"))
+
+#numeros -float -integer -complex
+#booleanos
